5fbf73c34488a10a042bbe8db5552b4c1ad4ff6b
christian-fe <christian.se@globant.com>
2021-11-12 14:41:56 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebMeme.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
